94fba6e1672b3bafd3c6eaf804678ae72b063d7d
ebuisson <99840432+ebuisson@users.noreply.github.com>
2024-05-01 15:10:37 -0400

    Add files via upload

---
+++ best-bidirectional-hits.py
@@ -0,0 +1,121 @@
+import csv
+import pandas as pd
+
+'''
+Function to find orthologous and co-orthologous protein sequences using Best Bidirectional Hits method
+input = blastp results sorted by increasing e-value for each query protein
+output = file containing Best Bidirectional Hits; two files with in-paralogs for each of two species
+'''
+
+def orthologs(blastp_1vs2, blastp_2vs1, blastp_1vs1, blastp_2vs2):
+#create empty dataframes for each blastp query
+    df1 = pd.DataFrame(columns=['MP','GO','eval'])
+    df2 = pd.DataFrame(columns=['GO','MP','eval'])
+    df3 = pd.DataFrame(columns=['MP1','MP2','eval_ip','in_paralog'])
+    df4 = pd.DataFrame(columns=['GO1','GO2','eval_ip','in_paralog'])
+
+#for all files, choose only the match with the lowest e-value for each protein pair
+    with open(blastp_1vs2) as my_file1:
+        tsv_reader = csv.reader(my_file1, delimiter="\t")
+        id = ''
+        for row in tsv_reader: 
+            if row[0] == id:
+                continue
+            else:
+                new_row={'MP':row[0],'GO':row[1],'eval':row[10]}
+                df1 = df1.append(new_row, ignore_index=True)
+                id = row[0]
+
+    with open(blastp_2vs1) as my_file2:
+        tsv_reader = csv.reader(my_file2, delimiter="\t")
+        id = ''
+        for row in tsv_reader: 
+            if row[0] == id:
+                continue
+            else:
+                new_row={'GO':row[0],'MP':row[1],'eval':row[10]}
+                df2 = df2.append(new_row, ignore_index=True)
+                id = row[0]
+     
+    with open(blastp_1vs1) as my_file3:
+        tsv_reader = csv.reader(my_file3, delimiter="\t")
+        id = ''
+        for row in tsv_reader: 
+            #ignore rows that match with themselves
+            if row[0] == row[1]:
+                continue
+            elif row[0] == id:
+                continue
+            else:
+                new_row={'MP1':row[0],'MP2':row[1],'eval_ip':row[10],'in_paralog':False}
+                df3 = df3.append(new_row, ignore_index=True)
+                id = row[0]
+
+    with open(blastp_2vs2) as my_file4:
+        tsv_reader = csv.reader(my_file4, delimiter="\t")
+        id = ''
+        for row in tsv_reader: 
+            #ignore rows that match with themselves
+            if row[0] == row[1]:
+                continue
+            elif row[0] == id:
+                continue
+            else:
+                new_row={'GO1':row[0],'GO2':row[1],'eval_ip':row[10],'in_paralog':False}
+                df4 = df4.append(new_row, ignore_index=True)
+                id = row[0]
+    
+    #find pairs appearing in both best hit files
+    column_titles = ['MP','GO']
+    df2_new=df2.reindex(columns=column_titles)
+
+    BBH = pd.concat([df1,df2_new]).groupby(['GO','MP']).size()
+    BBH = BBH[BBH == 2]
+
+    #find in-paralogs 
+    #...in species 1
+    for index1, row1 in df3.iterrows():
+        for index2, row2 in df1.iterrows():
+            if row1['MP1'] == row2['MP'] and float(row1['eval_ip']) < float(row2['eval']):
+                row1['in_paralog'] = True
+            else:
+                continue
+            
+    #...in species 2
+    for index1, row1 in df4.iterrows():
+        for index2, row2 in df2.iterrows():
+            if row1['GO1'] == row2['GO'] and float(row1['eval_ip']) < float(row2['eval']):
+                row1['in_paralog'] = True
+            else:
+                continue
+
+    return BBH, df3, df4
+
+
+
+
+
+
+file1 = '/Users/Emilie/Desktop/Reg_Gen/sorted.1_results.txt'
+file2 = '/Users/Emilie/Desktop/Reg_Gen/sorted.2_results.txt'
+file3 = '/Users/Emilie/Desktop/Reg_Gen/sorted.MP_results.txt'
+file4 = '/Users/Emilie/Desktop/Reg_Gen/sorted.GO_results.txt'
+results = orthologs(file1, file2, file3, file4)
+
+results[4].to_csv(r'/Users/Emilie/Desktop/Reg_Gen/myBBH.txt', header=None, index=None, sep = '\t')
+#results[2].to_csv(r'/Users/Emilie/Desktop/Reg_Gen/besthits_4.txt', header=None, index=None, sep = '\t')
+
+#if I want output...        
+results[0].to_csv(r'/Users/Emilie/Desktop/Reg_Gen/mytest_1.txt', header=None, index=None, sep = '\t')
+results[1].to_csv(r'/Users/Emilie/Desktop/Reg_Gen/mytest_2.txt', header=None, index=None, sep = '\t')
+results[2].to_csv(r'/Users/Emilie/Desktop/Reg_Gen/mytest_3.txt', header=None, index=None, sep = '\t')
+results[3].to_csv(r'/Users/Emilie/Desktop/Reg_Gen/mytest_4.txt', header=None, index=None, sep = '\t')
+
+
+
+
+
+
+
+
+
---
+++ shannon-entropy.py
@@ -0,0 +1,99 @@
+import csv
+import matplotlib.pyplot as plt
+from collections import Counter
+import math
+
+'''
+Function to identify conserved regions in a Multiple Sequence Alignment file with Shannon Entropy method
+input = MSA file in .aln format
+output = list with Shannon entropy values for each position in the MSA file
+'''
+
+def shannon_entropy(aa_counts):
+    # gaps included as a 21st AA
+    # positions with >50% gaps are ignored (output = -1)
+    # pi = fraction of residues of each aa type
+    # M = number of amino acid types
+    H = 0
+    residues_count = int(sum(aa_counts.values()))
+    if residues_count <= 13:
+        H = 1
+    else:
+        for value in aa_counts.values():
+            pi = int(value) / 27
+            H = H + (pi*math.log(pi,2))
+        if residues_count < 27:
+            gap_pi = (27-residues_count)/ 27
+            H = H + (gap_pi*math.log(gap_pi,2))
+    return -H
+
+#included to compare to Shannon entropy scores
+def variability(N,k,freq):
+    # excluding gaps
+    # N = the number of sequences in the alignment
+    # k = the number of different amino acids at a given position
+    # n = the frequency of the most common amino acid at that position
+    return((N*k)/freq)
+
+#make a dictionary with key=species and value=string of protein sequence with gaps
+def make_dict(file):
+    dict = {}
+    with open(file) as my_file:
+        tsv_reader = csv.reader(my_file, delimiter=" ", skipinitialspace=True)
+        next(tsv_reader)
+        next(tsv_reader)
+        next(tsv_reader)
+        for row in tsv_reader: 
+            if len(row)==0 or len(row[0])==0 or row[0].startswith(tuple([' ','.',':','*'])):
+                continue
+            if dict.get(row[0]) is None:
+                dict.update({row[0]:''})
+            dict[row[0]]=dict[row[0]] + row[1]
+    return dict
+
+#to plot resulting entropy values; values between 0 and 1 (inclusive) will be plotted in red
+def barplot(y_values):
+    print(y_values)
+    x_axis = range(len(y_values))
+    y_axis = y_values
+    col = []
+    for num in y_axis:
+        if num < 0 or num > 1:
+            col.append('blue')
+        else:
+            col.append('red')
+    plt.bar(x_axis, y_axis, color=col)
+    plt.xlabel('MSA index')
+    plt.ylabel('Shannon entropy')
+    plt.show()
+
+def functional_regions(dict):
+    len_seq = int(sum(len(x) for x in dict.values())/len(dict))
+    var = []
+    shannon = []
+    for i in range(len_seq):
+        str = ''
+        for value in dict.values(): 
+            x = list(value)
+            if x[i] == '-':
+                continue
+            else:
+                str = str + x[i]
+        s = set(str)
+        c = Counter(str)
+        v = variability(len(str), len(s), (c.most_common(1)[0][1])/len(str))
+        var.append(v)
+        se = shannon_entropy(c)
+        shannon.append(se)
+        str=''
+    return(shannon)
+
+file = '/Users/Emilie/Desktop/Reg_Gen/sequences_yes.aligned.aln'
+barplot(functional_regions(make_dict(file)))
+        
+    
+    
+    
+
+
+        
